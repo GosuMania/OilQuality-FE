b45d818110de3ee39aca8c5ef3ae198f8f5d352f
DESKTOP-PG4JPP7\luigi <luigipedata1991@gmail.com>
2023-01-05 18:27:30 +0100

    Prima push con modifiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oilQuality';
+  name = 'This is XLSX TO JSON CONVERTER';
+  loading = false;
+  willDownload = false;
+  oilResult = {
+    rating: '',
+    probability: 0
+  };
+  @ViewChild('fileInput') fileInput: any;
+  outputJson = '';
+  onClickFileInputButton(): void {
+    this.fileInput.nativeElement.click();
+  }
+  onFileChange(event: any) {
+    this.loading = true;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = event.target.files[0];
+    reader.onload = () => {
+      const data = reader.result;
+      const workBook = XLSX.read(data, {type: 'binary'});
+      jsonData = workBook.SheetNames.reduce((initial: any, name: string) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      this.outputJson = jsonData;
+
+      const dataString = JSON.stringify(jsonData);
+      // this.outputJson = dataString.slice(0, 300).concat("...");
+      this.setDownload(dataString);
+    }
+    reader.readAsBinaryString(file);
+  }
+
+
+  setDownload(data: string | number | boolean) {
+    setTimeout(() => {
+      const el = document.querySelector("#download");
+      if (el) {
+        el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+        el.setAttribute("download", 'xlsxtojson.json');
+      }
+      this.oilResult.rating = 'GOOD';
+      this.oilResult.probability = 99.1916;
+      this.loading = false;
+      this.willDownload = true;
+    }, 1000)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
